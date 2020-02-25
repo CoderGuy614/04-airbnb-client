ee965f787cde8e782ccbad4820f6f5120b35a8f6
CoderGuy614 <jlutz.110@gmail.com>
2020-02-25 13:38:34 +0800

    Final Adjustments to CSS

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -11,32 +11,29 @@ class Thumbnail extends React.Component {
 
   render() {
     return (
-      <>
+      <a
+        className="card link"
+        onMouseEnter={e => this.props.houseHover(this.state.house._id)}
+      >
         <div
-          className="card link"
-          onMouseEnter={e => this.props.houseHover(this.state.house._id)}
-        >
-          <div
-            className="image"
-            style={{ backgroundImage: `url('${this.state.house.image}')` }}
-          ></div>
-          <div className="content">
-            <small className="meta">
-              {this.state.house.type.name} • {this.state.house.bedrooms}{" "}
-              Bedrooms
-            </small>
-            <h2>{this.state.house.title}</h2>
-            <small className="location">
-              <i className="fas fa-map-marker-alt"></i>
-              <span>
-                {this.state.house.city}, {this.state.house.region}
-              </span>
-            </small>
-            <span className="price">${this.state.house.price}/night</span>
-            <StarRating rating={this.state.house.rating} />
-          </div>
+          className="image"
+          style={{ backgroundImage: `url('${this.state.house.image}')` }}
+        ></div>
+        <div className="content">
+          <small className="meta">
+            {this.state.house.type.name} • {this.state.house.bedrooms} Bedrooms
+          </small>
+          <h2>{this.state.house.title}</h2>
+          <small className="location">
+            <i className="fas fa-map-marker-alt"></i>
+            <span>
+              {this.state.house.city}, {this.state.house.region}
+            </span>
+          </small>
+          <span className="price">${this.state.house.price}/night</span>
+          <StarRating rating={this.state.house.rating} />
         </div>
-      </>
+      </a>
     );
   }
 }
